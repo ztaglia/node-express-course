13910a4ec0e9ebe075335aa3ff155dde3a5b9ce7
ztaglia <ztaglia@umich.edu>
2024-12-25 12:43:19 -0600

    added rest api

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,19 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks')
+
+// middleware 
+app.use(express.json())
+
+
+// routes
+app.get('/hello', (req, res)=>{
+    res.send('Task Manager App')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+
+const port = 3000;
+
+app.listen(port, console.log(`server is listening on port ${port}...`))
